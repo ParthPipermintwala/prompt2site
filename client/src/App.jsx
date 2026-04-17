2c2163d50331f32c77e9179dd6df5a23a86201cb
parth <parthpipermintwala9@gmail.com>
2026-04-17 23:35:40 +0530

    basic home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import Particles from "./components/Particles";
+import { Outlet } from "react-router-dom";
+import "./index.css";
 
 export default function App() {
   return (
     <>
-
-      <div className="absolute top-0 left-0 w-screen h-screen">
+      <div className="absolute top-0 left-0 w-screen h-screen -z-10 bg-[#120F17]">
         <Particles
-          particleColors={["#ffffff"]}
-          particleCount={200}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
+          particleColors={["#8FDFFF"]}
+          particleCount={400}
+          particleSpread={20}
+          speed={0.2}
+          particleBaseSize={150}
           moveParticlesOnHover
           alphaParticles={false}
           disableRotation={false}
-          pixelRatio={1}
+          pixelRatio={2}
         />
       </div>
-    
+      <Outlet />
     </>
   );
 }
