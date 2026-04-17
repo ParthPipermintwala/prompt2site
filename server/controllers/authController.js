6602e75613f1e6239c19baf93b6a3ab4a765828b
parth <parthpipermintwala9@gmail.com>
2026-04-17 18:51:48 +0530

    fix login controll

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -103,14 +103,14 @@ export const signin = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).select("+password").lean();
+    const user = await User.findOne({ email }).select("+password");
     if (!user) {
       return res.status(400).json({
         message: "Invalid email or password",
       });
     }
 
-    const isMatch = user.verifyPassword(password);
+    const isMatch =await user.verifyPassword(password);
     if (!isMatch) {
       return res.status(400).json({
         message: "Invalid email or password",
