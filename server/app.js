a046c032b91e03a3fd55ec07a4727ccc58bc95f9
parth <parthpipermintwala9@gmail.com>
2026-04-17 20:00:31 +0530

    add routing in app.js

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
+app.disable("x-powered-by");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true, //allowing cookies to be sent with requests from the frontend
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 
+//Routing
+app.use("/api/auth", authRoutes);
 
+//global error handling middleware
+app.use((err, req, res, next) => {
+   res.status(err.status || 500).json({ message: "something went wrong" });
+});
 export default app;
