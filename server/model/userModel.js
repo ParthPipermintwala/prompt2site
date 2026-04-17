b357048e3e668bfea9a8c40a741e03ad98afc2fb
parth <parthpipermintwala9@gmail.com>
2026-04-17 14:45:07 +0530

    create userModel

---
+++ server/model/userModel.js
@@ -0,0 +1,36 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y",
+    },
+    credits: {
+      type: Number,
+      default: 100,
+      min: 0,
+    },
+    plan: {
+      type: String,
+      enum: ["free", "pro", "enterprise"],
+      default: "free",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
