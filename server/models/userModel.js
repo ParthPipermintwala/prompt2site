1e8b529bb0a5fcbd540c46006d1872270b6c9794
parth <parthpipermintwala9@gmail.com>
2026-04-17 17:28:58 +0530

    update email schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -13,6 +13,9 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/\S+@\S+\.\S+/, "Please use a valid email address"],
     },
     password: {
       type: String,
