7b1b418a92c047c0f721f664482e7fcb5a191574
parth <parthpipermintwala9@gmail.com>
2026-04-17 23:36:43 +0530

    add error page

---
+++ client/src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ErrorPage() {
+  return (
+    <div className='text-7xl flex text-center justify-center h-screen items-center text-white'>
+     ❌ 404 page not found
+    </div>
+  )
+}
