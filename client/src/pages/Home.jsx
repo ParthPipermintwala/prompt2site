d21f3bb4363e1a99b58690e7222fab235a2f9392
parth <parthpipermintwala9@gmail.com>
2026-04-18 11:19:57 +0530

    refactor Home component to replace Motion.div with Navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,10 @@
+import Navbar from "@/components/Home/Navbar";
 import React from "react";
-import { motion as Motion } from "motion/react";
+
 export default function Home() {
   return (
     <div className="w-screen h-screen text-[#e1e8ea] hover:text-[#4FB8E6] transition duration-400 text-7xl">
-      <Motion.div className="bg-black w-full h-[10vh] flex justify-between items-center px-10 max-md:px-3">
-        <img src="/logo.svg" alt="logo" className="w-[15vw] max-md:w-[25vh]" />
-      </Motion.div>
+      <Navbar />
     </div>
   );
 }
