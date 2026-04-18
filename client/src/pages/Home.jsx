81c85864a601a32deb055d074454d0b66545e986
parth <parthpipermintwala9@gmail.com>
2026-04-18 22:38:10 +0530

    add Particles component to Home for enhanced background animation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,23 +5,39 @@ import Footer from "@/components/Home/Footer";
 import Highlightes from "@/components/Home/Highlightes";
 import Loginmodel from "./Loginmodel";
 import { AnimatePresence } from "framer-motion";
+import Particles from "@/components/animation/Particles";
 
 export default function Home() {
   const [openLogin, setOpenLogin] = React.useState(false);
   return (
-    <div className="max-w-screen max-h-screen overflow-x-hidden transition duration-400 text-7xl">
-      <Navbar OpenLogin={() => setOpenLogin(true)} />
-      <Herosection OpenLogin={() => setOpenLogin(true)} />
-      <Highlightes />
-      <Footer />
-      <AnimatePresence mode="wait">
-        {openLogin && (
-          <Loginmodel
-            isOpen={openLogin}
-            CloseLogin={() => setOpenLogin(false)}
-          />
-        )}
-      </AnimatePresence>
-    </div>
+    <>
+      <div className="max-w-screen max-h-screen overflow-x-hidden transition duration-400 text-7xl">
+        <Navbar OpenLogin={() => setOpenLogin(true)} />
+        <Herosection OpenLogin={() => setOpenLogin(true)} />
+        <Highlightes />
+        <Footer />
+        <AnimatePresence mode="wait">
+          {openLogin && (
+            <Loginmodel
+              isOpen={openLogin}
+              CloseLogin={() => setOpenLogin(false)}
+            />
+          )}
+        </AnimatePresence>
+      </div>
+      <div className="fixed inset-0 z-0 pointer-events-none">
+        <Particles
+          particleColors={["#6dbfdf"]}
+          particleCount={450}
+          particleSpread={20}
+          speed={0.4}
+          particleBaseSize={150}
+          moveParticlesOnHover
+          alphaParticles={false}
+          disableRotation={false}
+          pixelRatio={1}
+        />
+      </div>
+    </>
   );
 }
