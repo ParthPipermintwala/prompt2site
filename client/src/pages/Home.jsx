ca70703f9d98c187a9342f43affbe76d15c1fa36
parth <parthpipermintwala9@gmail.com>
2026-04-18 11:47:57 +0530

    refactor Home component to remove hover text color and simplify class names

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen text-[#e1e8ea] hover:text-[#4FB8E6] transition duration-400 text-7xl">
+    <div className="w-screen h-screen transition duration-400 text-7xl">
       <Navbar />
     </div>
   );
