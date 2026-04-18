a943c40e87ff915545e1035fd471c5d94428a90f
parth <parthpipermintwala9@gmail.com>
2026-04-18 21:43:07 +0530

    wrap App component with GoogleOAuthProvider for Google authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import Particles from "./components/animation/Particles";
 import Loader from "./components/common/Loader";
 import { Outlet, useNavigation } from "react-router-dom";
@@ -9,29 +10,32 @@ export default function App() {
   const isLoading = navigation.state !== "idle";
 
   return (
-    <div
-      className="relative min-h-screen isolate overflow-hiddenselect-none bg-[#120f17]"
-      onContextMenu={(e) => e.preventDefault()}
-    >
-      <div className="fixed inset-0 z-0 pointer-events-none">
-        <Particles
-          particleColors={["#6dbfdf"]}
-          particleCount={450}
-          particleSpread={20}
-          speed={0.4}
-          particleBaseSize={150}
-          moveParticlesOnHover
-          alphaParticles={false}
-          disableRotation={false}
-          pixelRatio={1}
-        />
-      </div>
-      {isLoading ? (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <div
+        className="relative min-h-screen isolate overflow-hidden select-none bg-[#120f17]"
+        onContextMenu={(e) => e.preventDefault()}
+      >
+        <div className="fixed inset-0 z-0 pointer-events-none">
+          <Particles
+            particleColors={["#6dbfdf"]}
+            particleCount={450}
+            particleSpread={20}
+            speed={0.4}
+            particleBaseSize={150}
+            moveParticlesOnHover
+            alphaParticles={false}
+            disableRotation={false}
+            pixelRatio={1}
+          />
+        </div>
+        {isLoading ? (
           <Loader />
-      ) : null}
-      <div className="relative z-10">
-        <Outlet />
+        ) : (
+          <div className="relative z-10">
+            <Outlet />
+          </div>
+        )}
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
