24d9f0b2b8a97af38d647474beee1014c3523723
parth <parthpipermintwala9@gmail.com>
2026-04-18 21:43:25 +0530

    update Navbar component to accept OpenLogin prop and wrap navigation links in Motion divs for improved animation

--- client/src/components/Home/Navbar.jsx
+++ client/src/components/Home/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion as Motion } from "motion/react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({ OpenLogin }) {
   return (
     <Motion.div
       initial={{ y: -70, opacity: 0 }}
@@ -10,20 +10,34 @@ export default function Navbar() {
       transition={{ duration: 0.5, ease: "easeInOut" }}
       className="fixed inset-0 w-full h-[8vh] flex justify-between items-center px-10 max-md:px-3 cursor-pointer"
     >
-      <img src="/logo.svg" alt="logo" className="w-[18vw] max-md:w-[28vh]" />
+      {/* // Logo */}
+      <Link to="/">
+        <Motion.img
+          whileTap={{ scale: 0.7 }}
+          src="/logo.svg"
+          alt="logo"
+          className="w-[18vw] max-md:w-[28vh]"
+        />
+      </Link>
+      {/* // Navigation Links */}
       <div className="flex gap-10 max-md:gap-5 text-[2.5vh] max-md:hidden">
-        <Link
-          to="#pricing"
-          className="text-[#e1e8ea] hover:text-[#13b6f6d9] transition duration-400"
-        >
-          Pricing
-        </Link>
-        <Link
-          to="#services"
-          className="text-[#e1e8ea] hover:text-[#13b6f6d9] transition duration-400"
-        >
-          Get Started
-        </Link>
+        <Motion.div whileTap={{ scale: 0.6 }}>
+          <Link
+            to="#pricing"
+            className="text-[#e1e8ea] hover:text-[#13b6f6d9] transition duration-400"
+          >
+            Pricing
+          </Link>
+        </Motion.div>
+        <Motion.div whileTap={{ scale: 0.6 }}>
+          <Link
+            to="#services"
+            className="text-[#e1e8ea] hover:text-[#13b6f6d9] transition duration-400"
+            onClick={OpenLogin}
+          >
+            Get Started
+          </Link>
+        </Motion.div>
       </div>
     </Motion.div>
   );
