327d7937e5bad8f90ef464184d87c768d12b1c9f
parth <parthpipermintwala9@gmail.com>
2026-04-18 21:43:33 +0530

    implement login modal functionality in Home component with animation support

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,25 @@ import Navbar from "@/components/Home/Navbar";
 import Herosection from "@/components/Home/Herosection";
 import Footer from "@/components/Home/Footer";
 import Highlightes from "@/components/Home/Highlightes";
+import Loginmodel from "./Loginmodel";
+import { AnimatePresence } from "framer-motion";
 
 export default function Home() {
+  const [openLogin, setOpenLogin] = React.useState(false);
   return (
     <div className="max-w-screen max-h-screen overflow-x-hidden transition duration-400 text-7xl">
-      <Navbar />
-      <Herosection />
+      <Navbar OpenLogin={() => setOpenLogin(true)} />
+      <Herosection OpenLogin={() => setOpenLogin(true)} />
       <Highlightes />
       <Footer />
+      <AnimatePresence mode="wait">
+        {openLogin && (
+          <Loginmodel
+            isOpen={openLogin}
+            CloseLogin={() => setOpenLogin(false)}
+          />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
