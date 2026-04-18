80f68c30fc86b6229e09ad0a6cd5c3d224c2803d
parth <parthpipermintwala9@gmail.com>
2026-04-19 00:57:48 +0530

    remove unused Signup import from router

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
-import Signup from "./pages/signup";
 import ErrorPage from "./components/common/ErrorPage";
 import Loginmodel from "./pages/Loginmodel";
 
