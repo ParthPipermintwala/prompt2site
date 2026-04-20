1791a5314d41cc2f1a40f54c9ad260d4003cb638
parth <parthpipermintwala9@gmail.com>
2026-04-20 22:37:50 +0530

    feat: add react-icons dependency and integrate GitHub icon in Footer component

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -17,8 +18,10 @@ export default function Footer() {
         href="https://github.com/ParthPipermintwala/prompt2site"
         target="_blank"
         rel="noopener noreferrer"
-        className="inline-flex max-md:text-[12px] h-[5vh] items-center rounded-full border border-white/10 bg-white/5 px-4 py-2 text-blue-300 transition duration-300 ease-in-out hover:border-cyan-400/40 hover:bg-cyan-400/10 hover:text-cyan-200 max-md:px-2 max-md:py-1"
+        className="inline-flex max-md:text-[13px] h-[5vh] max-md:h-[6vh] items-center rounded-full border border-white/10 bg-white/5 px-4 py-2 text-blue-300 transition duration-300 ease-in-out hover:border-cyan-400/40 hover:bg-cyan-400/10 hover:text-cyan-200 max-md:px-2 max-md:py-1"
       >
+        <FaGithub size={24} />
+        &nbsp;
         prompt2site GitHub
       </a>
     </footer>
