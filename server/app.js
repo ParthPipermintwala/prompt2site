7e11405438729fcc3c32640a83240026cd2b66e7
parth <parthpipermintwala9@gmail.com>
2026-04-22 18:17:03 +0530

    update routing to include user routes with authentication middleware

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/authRoutes.js";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 app.disable("x-powered-by");
@@ -16,10 +18,12 @@ app.use(cookieParser());
 app.use(express.json());
 
 //Routing
-app.use("/api/auth", authRoutes);
+app.use("/api/auth",authRouter);
+app.use("/api/user",authMiddleware,userRouter);
 
 //global error handling middleware
 app.use((err, req, res, next) => {
    res.status(err.status || 500).json({ message: "something went wrong" });
 });
+
 export default app;
