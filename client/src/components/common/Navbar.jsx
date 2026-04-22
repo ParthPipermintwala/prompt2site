8487198481b76341104bc85a4fa334f15defafb9
parth <parthpipermintwala9@gmail.com>
2026-04-23 00:34:35 +0530

    feat: enhance Navbar component with profile dropdown and login icon

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import { motion as Motion } from "motion/react";
+import React, { useState } from "react";
+import { AnimatePresence, motion as Motion } from "motion/react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { CoinsIcon } from "lucide-react";    
+import { CoinsIcon, LogInIcon } from "lucide-react";
+import Profile from "./Profile";
 
 export default function Navbar({ OpenLogin }) {
   const { userData } = useSelector((state) => state.user);
+  const [openProfile, setOpenProfile] = useState(false);
 
   return (
     <Motion.div
@@ -23,8 +25,11 @@ export default function Navbar({ OpenLogin }) {
         />
       </Link>
 
-      <div className="flex gap-9 max-md:gap-3 max-md:text-sm items-center mr-2">
-        <Motion.div whileTap={{ scale: 0.8 }} className="max-md:hidden text-[24px] bold">
+      <div className="flex gap-7 max-md:gap-3 max-md:text-sm items-center mr-2">
+        <Motion.div
+          whileTap={{ scale: 0.8 }}
+          className="max-md:hidden text-[24px] bold"
+        >
           <Link
             to="#pricing"
             className="text-[#e1e8ea] hover:text-[#13b6f6d9] transition duration-200"
@@ -33,35 +38,45 @@ export default function Navbar({ OpenLogin }) {
           </Link>
         </Motion.div>
 
-      {userData && (<Motion.div whileTap={{ scale: 0.9 }}  className="flex items-center gap-1 rounded-full bg-white/5 border border-white/10 text-lg max-md:text-[3vw] semi-bold px-3 py-1.5 hover:bg-white/10 max-md:px-2 max-md:py-1">
-          <CoinsIcon size={20} className="text-yellow-400"/>
-          <span className="text-zinc-300">Credits{" "}</span>
-          <span className="bold">{userData.credits}</span>
-          <span>+</span>
-      </Motion.div>)}
+        {userData && (
+          <Motion.div
+            whileTap={{ scale: 0.9 }}
+            className="flex items-center gap-1 rounded-full bg-white/5 border border-white/10 text-lg max-md:text-[3vw] semi-bold px-2 py-1 hover:bg-white/10"
+          >
+            <CoinsIcon size={20} className="text-yellow-400" />
+            <span className="text-zinc-300">Credits </span>
+            <span className="bold">{userData.credits}</span>
+            <span>+</span>
+          </Motion.div>
+        )}
 
         {!userData ? (
           <Motion.div whileTap={{ scale: 0.8 }}>
             <div
-              className="text-[#e1e8ea] hover:bg-[#034b68d1] transition duration-200 max-md:text-sm text-[24px] bold border-2 border-[#13b6f6d9] px-4 py-2 rounded-xl bg-[#13b6f6d9]/20 max-md:px-2 max-md:py-1 max-md:mt-2"
+              className="flex items-center text-[#e1e8ea] hover:bg-[#034b68d1] transition duration-200 max-md:text-sm text-[20px] bold border-2 border-[#13b6f6d9] px-2 py-1.5 rounded-xl bg-[#13b6f6d9]/20  max-md:py-1 max-md:mt-2"
               onClick={OpenLogin}
             >
+              <LogInIcon size={23} className="inline-block mr-2"/>
               Get Started
             </div>
           </Motion.div>
         ) : (
-          <Motion.div whileTap={{ scale: 0.8 }} className="flex items-start ">
-            <Link to="#profile">
+          <div>
+            <Motion.div
+              whileTap={{ scale: 0.8 }}
+              onClick={() => setOpenProfile(!openProfile)}
+            >
               <img
                 src={
                   userData.avatar ||
                   `https://ui-avatars.com/api/?background=7e57c2&color=fff&name=${userData.name}`
                 }
                 alt="no profile"
-                className="w-9 h-9 rounded-full border-white/20 object-cover border-1"
+                className="w-10 h-10 max-md:w-9 max-md:h-9 rounded-full border-white/20 object-cover border-1"
               />
-            </Link>
-          </Motion.div>
+            </Motion.div>
+            <AnimatePresence>{openProfile && <Profile />}</AnimatePresence>
+          </div>
         )}
       </div>
     </Motion.div>
