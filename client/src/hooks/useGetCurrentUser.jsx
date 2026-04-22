e1dbc3a5858ca1fe6946a311b9ffd8b00ed69686
parth <parthpipermintwala9@gmail.com>
2026-04-22 22:52:06 +0530

    feat: update useGetCurrentUser hook to dispatch user data to Redux store

--- client/src/hooks/useGetCurrentUser.jsx
+++ client/src/hooks/useGetCurrentUser.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { setUserData } from "@/features/user/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function useGetCurrentUser() {
+  const dispatch = useDispatch();
   useEffect(() => {
     const getCurrentUser = async () => {
       try {
@@ -9,7 +12,7 @@ export default function useGetCurrentUser() {
         const {data} = await axios.get(`${baseUrl}/api/user/profile`, {
           withCredentials: true,
         });
-        return data;
+        dispatch(setUserData(data));
       } catch (error) {
         console.error("Error fetching current user:- ", error);
       }
