e6081649e63f1476cb807c4b4ffbd00a1e4d5e45
parth <parthpipermintwala9@gmail.com>
2026-04-22 20:15:47 +0530

    refactor: streamline loading state management in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import { Outlet, useNavigation } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Loader from "./components/common/Loader";
-import { Outlet, useNavigation } from "react-router-dom";
-import "./index.css";
-
-const subscribeToBootState = (callback) => {
-  window.addEventListener("load", callback);
-  return () => window.removeEventListener("load", callback);
-};
-
-const getBootState = () => document.readyState !== "complete";
+import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 export default function App() {
   const navigation = useNavigation();
-  const isBootLoading = React.useSyncExternalStore(
-    subscribeToBootState,
-    getBootState,
-    () => true,
-  );
-  const isLoading = isBootLoading || navigation.state !== "idle";
+  //only for first load
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
 
+  useEffect(() => {
+    const handleLoad = () => setIsInitialLoading(false);
+
+    window.addEventListener("load", handleLoad);
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  //consider loading when the app is first loaded or when navigating to a new route
+  const isLoading = isInitialLoading || navigation.state === "loading";
+
+  useGetCurrentUser();
   return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      {isLoading && <Loader />}
-
-      <div
-        className="relative z-10 min-h-screen isolate overflow-hidden select-none bg-[#120f17]"
-        onContextMenu={(e) => e.preventDefault()}
-      >
-        <Outlet />
-      </div>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        {isLoading && <Loader />}
+        <div
+          className="relative z-10 min-h-screen isolate overflow-hidden select-none bg-[#120f17]"
+          onContextMenu={(e) => e.preventDefault()}
+        >
+          <Outlet />
+        </div>
+      </GoogleOAuthProvider>
+    </Provider>
   );
 }
