1a44a512a25a4f36cf6f5fa95fb2aa8bb45f8380
parth <parthpipermintwala9@gmail.com>
2026-04-30 00:20:42 +0530

    feat: add authentication validation for dashboard and generate routes

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,6 +2,15 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import ErrorPage from "./components/common/ErrorPage";
+import Dashboard from "./pages/Dashboard";
+import Genrate from "./pages/Genrate";
+import { store } from "./app/store";
+
+const validateAuth = () => {
+  const userData = store.getState().user.userData;
+  if (!userData) redirect("/");
+  return null;
+};
 
 const router = createBrowserRouter([
   {
@@ -13,6 +22,16 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        loader: validateAuth,
+        element: <Dashboard />,
+      },
+      {
+        path: "/generate",
+        loader: validateAuth,
+        element: <Genrate />,
       },
       {
         path: "*",
