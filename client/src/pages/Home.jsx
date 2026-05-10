dd668ede0720872c1f2c3c9cab2fb892fdc9aff5
parth <parthpipermintwala9@gmail.com>
2026-05-10 20:10:40 +0530

    fix: correct import path for Loginmodel component in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "@/components/common/Navbar";
 import Herosection from "@/components/Home/Herosection";
 import Footer from "@/components/common/Footer";
 import Highlightes from "@/components/Home/Highlightes";
-import Loginmodel from "../components/common/Loginmodel";
+import Loginmodel from "../components/Home/Loginmodel";
 import { AnimatePresence } from "framer-motion";
 import Particles from "@/components/animation/Particles";
 import useGetCurrentUser from "@/hooks/useGetCurrentUser";
+import Navbar from "@/components/Home/Navbar";
 
 export default function Home() {
   const [openLogin, setOpenLogin] = React.useState(false);
