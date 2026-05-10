7e015a9e5078366876e9cc3269533b12bc4b9b03
parth <parthpipermintwala9@gmail.com>
2026-05-10 19:50:09 +0530

    style: enhance Profile component with animations and layout improvements

--- client/src/components/Home/Profile.jsx
+++ client/src/components/Home/Profile.jsx
@@ -22,35 +22,53 @@ export default function Profile() {
 
   return (
     <Motion.div
-      initial={{ opacity: 0, y: -100, scale: 0.9 }}
-      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: -100, scale: 0.5 }}
+      animate={{ opacity: 1, y: 0, scale: 0.9 }}
       transition={{ duration: 0.3, ease: "easeInOut" }}
-      exit={{ opacity: 0, y: 100, scale: 0.9 }}
+      exit={{ opacity: 0, x: 100, scale: 0.5 }}
       className="absolute right-1 mt-3 max-md:w-60 w-70 z-50 rounded-xl bg-[#0b0b0b] border border-white/10 shadow-2xl overflow-hidden"
     >
       <div className="px-4 py-3 border-b border-white/10">
-        <p className="text-lg font-medium truncate tracking-tight max-md:text-sm">
+        <Motion.p
+          initial={{ opacity: 0, y: -150 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2, ease: "easeInOut",delay: 0.2 }}
+          className="text-lg font-medium truncate tracking-tight max-md:text-sm"
+        >
           {userData.name}
-        </p>
-        <p className="text-sm text-zinc-400 truncate max-md:text-xs">
+        </Motion.p>
+        <Motion.p
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.2, ease: "easeInOut",delay: 0.15 }}
+          className="text-sm text-zinc-400 truncate max-md:text-xs"
+        >
           {userData.email}
-        </p>
+        </Motion.p>
       </div>
       <div className="flex flex-col gap-0">
-        <button
-          className="w-full text-left px-4 py-2 hover:bg-white/5 max-md:text-sm text-lg"
+        <Motion.button
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -100, opacity: 0 }}
+          transition={{ duration: 0.2, ease: "easeInOut", delay: 0.1 }}
+          className="cursor-pointer w-full text-left px-4 py-2 hover:bg-white/5 max-md:text-sm text-lg"
           onClick={() => navigate("/dashboard")}
         >
           <LayoutDashboard size={16} className="inline block mr-1" />
           Dashboard
-        </button>
-        <button
-          className="w-full text-left px-4 py-2 text-red-400 hover:bg-white/5 max-md:text-sm text-lg"
+        </Motion.button>
+        <Motion.button
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -100, opacity: 0 }}
+          transition={{ duration: 0.2, ease: "easeInOut", delay: 0.15 }}
+          className="cursor-pointer w-full text-left px-4 py-2 text-red-400 hover:bg-white/5 max-md:text-sm text-lg"
           onClick={handleLogout}
         >
           <LogOut size={16} className="inline block mr-1" />
           Logout
-        </button>
+        </Motion.button>
       </div>
     </Motion.div>
   );
