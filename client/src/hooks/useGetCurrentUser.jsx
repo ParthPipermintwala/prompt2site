a4a2aba108ec606bd999a783f11167b736be9815
parth <parthpipermintwala9@gmail.com>
2026-05-10 20:10:10 +0530

    fix: improve user data retrieval logic in useGetCurrentUser hook

--- client/src/hooks/useGetCurrentUser.jsx
+++ client/src/hooks/useGetCurrentUser.jsx
@@ -6,10 +6,14 @@ import { useDispatch } from "react-redux";
 export default function useGetCurrentUser() {
   const dispatch = useDispatch();
   useEffect(() => {
+    if (localStorage.getItem("user")) {
+      dispatch(setUserData(JSON.parse(localStorage.getItem("user"))));
+      return;
+    }
     const getCurrentUser = async () => {
       try {
         const baseUrl = import.meta.env.VITE_BACKEND_URL;
-        const {data} = await axios.get(`${baseUrl}/api/user/profile`, {
+        const { data } = await axios.get(`${baseUrl}/api/user/profile`, {
           withCredentials: true,
         });
         dispatch(setUserData(data));
