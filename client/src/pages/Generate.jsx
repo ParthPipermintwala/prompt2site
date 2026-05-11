435d2807fe9d249b01bfcbe86b3582415da56da5
parth <parthpipermintwala9@gmail.com>
2026-05-11 23:04:31 +0530

    seprate prompt box

--- client/src/pages/Generate.jsx
+++ client/src/pages/Generate.jsx
@@ -1,24 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion as Motion } from "motion/react";
 import Navbar from "@/components/Generate/Navbar";
-import axios from "axios";
+import Promptbox from "@/components/Generate/Promptbox";
 
 export default function Generate() {
-  const [prompt, setPrompt] = useState("");
-  const handlegenerate = async () => {
-    try {
-      const result = await axios.post(
-        `${import.meta.env.VITE_BACKEND_URL}/api/website/generate`,
-        { prompt },
-        {
-          withCredentials: true,
-        },
-      );
-      console.log(result.data);
-    } catch (error) {
-      console.error("Error generating website:", error);
-    }
-  };
+
   return (
     <div>
       <Navbar />
@@ -49,32 +35,7 @@ export default function Generate() {
           >
             Describe Your Website
           </Motion.h1>
-          <div className="relative">
-            <Motion.textarea
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.4, delay: 0.2, ease: "easeInOut" }}
-              placeholder="Describe your website in detail..."
-              className="w-full h-35 max-md:h-24 p-4 mb-5 rounded-2xl overflow-y-scroll hide-scrollbar bg-[#0c0a0f] border border-white/10 outline-none resize-none text-[16px] leading-relaxed focus:ring-1 focus:ring-white/30 "
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            ></Motion.textarea>
-          </div>
-          <Motion.div
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3, delay: 0.2, ease: "easeInOut" }}
-            className="flex justify-center"
-          >
-            <Motion.button
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.9 }}
-              className="cursor-pointer px-5 py-2 rounded-2xl font-semibold bg-white text-black text-lg"
-              onClick={handlegenerate}
-            >
-              Generate Website
-            </Motion.button>
-          </Motion.div>
+          <Promptbox />
         </div>
       </div>
     </div>
