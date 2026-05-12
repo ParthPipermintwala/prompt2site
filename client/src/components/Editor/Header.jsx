5259e3d2d483e2361736d285254c8a54833e16fe
parth <parthpipermintwala9@gmail.com>
2026-05-12 23:30:41 +0530

    feat: header of website

--- client/src/components/Editor/Header.jsx
+++ client/src/components/Editor/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import {motion as Motion} from "motion/react";
+import { Link } from "react-router-dom";
 
-export default function Header() {
+export default function Header({title = "Editor"}) {
   return (
-    <div>
-      
+    <div className="h-14 px-4 flex items-center justify-between border-b border-white/10">
+     <span className="font-semibold truncate">{title}</span>
     </div>
-  )
+  );
 }
