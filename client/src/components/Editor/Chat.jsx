df3d7d80d2c2b1918054ad9b94d438e1451efa4a
parth <parthpipermintwala9@gmail.com>
2026-05-12 23:30:24 +0530

    add converstion box

--- client/src/components/Editor/Chat.jsx
+++ client/src/components/Editor/Chat.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function Chat() {
+export default function Chat({conversations = []}) {
   return (
-    <div>
-      
+    <div className='flex-1 overflow-y-auto px-4 py-4 space-y-4'>
+      {
+        conversations.map((conversation, index) => (
+          <div key={index} className={`flex ${conversation.role === "user" ? "justify-end" : "justify-start"}`}>
+            <div className={`px-4 py-2.5 rounded-2xl text-sm  leading-relaxed ${conversation.role === "user" ? "bg-[#363636] text-zinc-300 text-right" : "border border-white/20 bg-[#2c2626] text-zinc-200 text-left"}`}>
+              {conversation.content}
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
