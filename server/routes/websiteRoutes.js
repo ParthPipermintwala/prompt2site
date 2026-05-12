59fad3ea89048825f385ad54380e80891bc29898
parth <parthpipermintwala9@gmail.com>
2026-05-12 19:47:50 +0530

    add getWebsites route

--- server/routes/websiteRoutes.js
+++ server/routes/websiteRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { generatewebsite } from "../controllers/websiteController.js";
+import { generatewebsite, getWebsites } from "../controllers/websiteController.js";
 
 const router = express.Router();
 
 router.post("/generate", generatewebsite);
+router.get("/websites/:id", getWebsites);
 
 export default router;
