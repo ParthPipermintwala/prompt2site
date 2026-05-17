69d76caa7c13ba284758cfaf706589ba72246843
parth <parthpipermintwala9@gmail.com>
2026-05-17 19:55:57 +0530

    add tap animation

--- client/src/components/Editor/CodeEditor.jsx
+++ client/src/components/Editor/CodeEditor.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import { motion as Motion } from "motion/react";
-import Editor from '@monaco-editor/react';
+import Editor from "@monaco-editor/react";
 import { X } from "lucide-react";
 import Loader from "../common/Loader";
 
-export default function CodeEditor({ setShowCode,code,setCode }) {
+export default function CodeEditor({ setShowCode, code, setCode }) {
   return (
     <Motion.div
-      initial={{ x: "100%" }}
+      initial={{ x: "-100%" }}
       animate={{ x: 0 }}
-      exit={{ x: "100%" }}
-      transition={{duration:0.2,ease:"easeInOut"}}
-      className="fixed inset-y-0 right-0 w-full lg:w-[45%] z-[9999]  flex flex-col"
+      exit={{ x: "-100%" }}
+      transition={{ duration: 0.2, ease: "easeInOut" }}
+      className="fixed inset-y-0 left-0 w-full lg:w-[38%] z-[9999]  flex flex-col"
     >
-      <Motion.div 
-      initial={{y:-80}}
-      animate={{y:0}}
-      transition={{duration:0.2,ease:"easeInOut",delay:0.1}}
-      className="h-12 px-4 flex justify-between items-center border-b border-white/10 bg-[#0b0a0a]">
+      <Motion.div
+        initial={{ y: -80 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.2, ease: "easeInOut", delay: 0.1 }}
+        className="h-12 px-4 flex justify-between items-center border-b border-white/10 bg-[#0b0a0a]"
+      >
         <span className="text-zinc-300">index.html</span>
-        <button onClick={() => setShowCode(false)} className="cursor-pointer">
-          <X size={18} />
-        </button>
+        <Motion.button
+          initial={{ scale: 1 }}
+          whileTap={{ scale: 0.8 }}
+          onClick={() => setShowCode(false)}
+          className="cursor-pointer"
+        >
+          <X size={22} />
+        </Motion.button>
       </Motion.div>
       <Editor
         theme="vs-dark"
         value={code}
         language="html"
-        loading={<Loader message="Loading Code ..."/>}
-        onChange={(val)=>setCode(val)}
+        loading={<Loader message="Loading Code ..." />}
+        onChange={(val) => setCode(val)}
       />
     </Motion.div>
   );
