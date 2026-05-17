4b2dd0e971d96071a09d398d3ce4ec1cb27450e6
parth <parthpipermintwala9@gmail.com>
2026-05-17 15:43:06 +0530

    feat: getAll route

--- server/routes/websiteRoutes.js
+++ server/routes/websiteRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { changeWebsite, generatewebsite, getWebsites } from "../controllers/websiteController.js";
+import { changeWebsite, generatewebsite, getAll, getWebsites } from "../controllers/websiteController.js";
 
 const router = express.Router();
 
 router.post("/generate", generatewebsite);
 router.get("/websiteData/:id", getWebsites);
 router.post("/changeWebsite/:id", changeWebsite);
+router.get("/getAll", getAll);
 
 export default router;
