0c837f44fe7cbc62b6ad022a0ae12235cc88d959
parth <parthpipermintwala9@gmail.com>
2026-05-18 14:54:46 +0530

    add error ui

--- client/src/components/Generate/Promptbox.jsx
+++ client/src/components/Generate/Promptbox.jsx
@@ -4,12 +4,15 @@ import axios from "axios";
 import { motion as Motion } from "motion/react";
 import { useDispatch, useSelector } from "react-redux";
 import { setUserData } from "@/features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function Promptbox() {
   const dispatch = useDispatch();
+  const navigate=useNavigate()
   const { userData } = useSelector((state) => state.user);
   const [prompt, setPrompt] = useState("");
   const [loading, setLoading] = useState(false);
+  const [Error, setError] = useState("");
   const [randomText, setRandomText] = useState("Generating...");
   const [seconds, setSeconds] = useState(0);
   const buttonText = [
@@ -52,18 +55,16 @@ export default function Promptbox() {
           "user",
           JSON.stringify({ ...userData, credits: result.data.creditsLeft }),
         );
+        navigate(`/editor/${result.data.websiteId}`)
       }
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      if (error.status === 403) {
-        alert(
-          "You don't have enough credits to generate a website. Please purchase more credits to continue.",
-        );
-        return;
-      }
+    } catch (error) { 
+      setError(error.response.data.message||"Something went wrong")
+      setTimeout(()=>{
+        setError("")
+      },3000)
       console.error("Error generating website:", error);
     } finally {
+      setLoading(false);
       clearInterval(interval);
     }
   };
@@ -78,24 +79,33 @@ export default function Promptbox() {
           transition={{ duration: 0.4, delay: 0.2, ease: "easeInOut" }}
           placeholder="Describe your website in detail..."
           readOnly={loading}
-          className={`w-full h-35 max-md:h-24 p-4 mb-5 rounded-2xl overflow-y-scroll hide-scrollbar bg-[#0c0a0f] border border-white/10 outline-none resize-none text-[16px] leading-relaxed focus:ring-1 focus:ring-white/30 ${
+          className={`w-full h-35 max-md:h-24 p-4 rounded-2xl overflow-y-scroll hide-scrollbar bg-[#0c0a0f] border border-white/10 outline-none resize-none text-[16px] leading-relaxed focus:ring-1 focus:ring-white/30 ${
             loading ? "cursor-not-allowed " : "cursor-text"
           }`}
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
         ></Motion.textarea>
+        {
+          Error && (
+            <Motion.div 
+            initial={{y:80,opacity:0}}
+            animate={{y:0,opacity:1}}
+            transition={{duration:0.2,ease:"easeInOut"}}
+            className="text-red-500 text-sm ml-2">* {" "}{Error}</Motion.div>
+          )
+        }
       </div>
       <Motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.3, delay: 0.2, ease: "easeInOut" }}
-        className="flex justify-center"
+        className="flex justify-center mt-6 transition"
       >
         <Motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.9 }}
           disabled={loading}
-          className={`px-5 py-2 rounded-2xl font-semibold bg-white text-black text-lg ${loading ? "cursor-not-allowed opacity-90" : "cursor-pointer"}`}
+          className={`px-5 py-2 rounded-2xl font-semibold bg-white text-black text-lg ${loading ? "cursor-not-allowed  bg-white/80 " : "cursor-pointer"}`}
           onClick={handlegenerate}
         >
           {loading ? randomText : "Generate Website"}
