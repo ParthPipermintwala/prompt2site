9518487eb6e1584495047deb273e1486734dcbd8
parth <parthpipermintwala9@gmail.com>
2026-05-20 20:14:00 +0530

    fix dotenv config issue

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
+import "dotenv/config";
 import app from "./app.js";
-import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
-dotenv.config();
 const port = process.env.PORT || 5000;
 try {
   await connectDB();
