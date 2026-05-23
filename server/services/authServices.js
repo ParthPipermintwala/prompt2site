5cec463b8ebe739b2a26e7c092e0e498e8f660d0
parth <parthpipermintwala9@gmail.com>
2026-05-23 14:35:45 +0530

    fix cookie

--- server/services/authServices.js
+++ server/services/authServices.js
@@ -88,8 +88,9 @@ export const setTokenCookie = async (res, jwtToken) => {
   // Set token in HTTP-only cookie
   res.cookie("token", jwtToken, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    secure: true,
+    sameSite: "none",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
