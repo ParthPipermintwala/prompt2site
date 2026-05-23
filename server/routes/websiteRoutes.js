5c649031ff2dcdf7aca44b1ee78cfbd8e1ee5f7a
parth <parthpipermintwala9@gmail.com>
2026-05-23 18:57:31 +0530

    add authmiddleware

--- server/routes/websiteRoutes.js
+++ server/routes/websiteRoutes.js
@@ -1,14 +1,23 @@
 import express from "express";
-import { changeWebsite, deleteWebsite, deploy, generatewebsite, getAll, getWebsites, getWebsitesBySlug } from "../controllers/websiteController.js";
+import {
+  changeWebsite,
+  deleteWebsite,
+  deploy,
+  generatewebsite,
+  getAll,
+  getWebsites,
+  getWebsitesBySlug,
+} from "../controllers/websiteController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/generate", generatewebsite);
-router.get("/websiteData/:id", getWebsites);
-router.post("/changeWebsite/:id", changeWebsite);
-router.get("/getAll", getAll);
-router.get("/deploy/:id", deploy);
+router.post("/generate", authMiddleware, generatewebsite);
+router.get("/websiteData/:id", authMiddleware, getWebsites);
+router.post("/changeWebsite/:id", authMiddleware, changeWebsite);
+router.get("/getAll", authMiddleware, getAll);
+router.get("/deploy/:id", authMiddleware, deploy);
 router.get("/getBySlug/:slug", getWebsitesBySlug);
-router.delete("/delete/:id", deleteWebsite);
+router.delete("/delete/:id", authMiddleware, deleteWebsite);
 
 export default router;
