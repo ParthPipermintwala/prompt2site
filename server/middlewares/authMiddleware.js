af808436b153dab74b148dbdacc55f95800c2bb3
parth <parthpipermintwala9@gmail.com>
2026-05-23 11:49:33 +0530

    cache user data

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,5 +1,10 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
+import {
+  cacheKeys,
+  getJsonCache,
+  setJsonCache,
+} from "../services/cacheService.js";
 
 export const authMiddleware = async (req, res, next) => {
   const token = req.cookies.token;
@@ -8,7 +13,21 @@ export const authMiddleware = async (req, res, next) => {
   }
   try {
     const { userId } = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(userId).select("-password -googleId").lean();
+    const userCacheKey = cacheKeys.user(userId);
+    const cachedUser = await getJsonCache(userCacheKey);
+
+    if (cachedUser) {
+      req.user = cachedUser;
+      return next();
+    }
+
+    const user = await User.findById(userId).select("-password -googleId").lean();
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    await setJsonCache(userCacheKey, user);
+    req.user = user;
     next();
   } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
