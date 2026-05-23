d6eda9b892e963194cc979ad557c3a851767fa48
parth <parthpipermintwala9@gmail.com>
2026-05-23 14:24:15 +0530

    debug

--- server/controllers/websiteController.js
+++ server/controllers/websiteController.js
@@ -267,6 +267,7 @@ export const getAll = async (req, res) => {
     await setJsonCache(cacheKey, website);
     return res.status(200).json(website);
   } catch (error) {
+    console.log(error)
     return res
       .status(500)
       .json({ message: "An error occurred while fetching websites" });
